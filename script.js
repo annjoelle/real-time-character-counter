0af38b7f95a720f701392d6356e35b0d1a51bfd8
annjoelle <132980118+annjoelle@users.noreply.github.com>
2025-03-27 23:35:08 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const textInput = document.getElementById("text-input");
+const charCount = document.getElementById("char-count");
+const clearButton = document.getElementById("clear-btn");
+
+textInput.addEventListener ("input", () => {
+    charCount.textContent = `Character Count: ${(textInput.value).length}/50`;
+    if ((textInput.value).length === 50) {
+        charCount.style.color = "red"; 
+    } else {
+        charCount.style.color = "rgb(53, 15, 66)";
+    }
+})
+
+clearButton.addEventListener ("click", () => {
+    charCount.textContent = `Character Count: 0/50`;
+    charCount.style.color = "rgb(53, 15, 66)";
+})
